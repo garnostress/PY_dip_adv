eda748352049df733a1ec6117c32b2bcb95a414d
garnostress <84721876+garnostress@users.noreply.github.com>
2021-12-30 16:39:06 +0300

    Init
    В переменной в файле settings.py необходимо изменить в строке пользователя, пароль, а также название базы данных.
    Также в файле есть функции get_user_token и get_token_group. в строке with open('group token', 'r') as file:
    вместо group token пишем название своего файла, куда скопировали токен.

---
+++ settings.py
@@ -0,0 +1,13 @@
+def get_token_group():
+    with open('group token', 'r') as file:
+        token = file.read()
+        return token
+
+
+def get_user_token():
+    with open('user token', 'r') as file:
+        token = file.read()
+        return token
+
+
+DSN = 'postgres://youuser:youpassword@localhost:5432/youdb'
